c0dd18ea7d3842ab0fb50898513fd549179b6410
SomeRandomDeveloper <git@srd.dev>
2026-04-16 17:49:07 +0200

    Add test for SpeedscopeProfile

---
+++ tests/phpunit/unit/SpeedscopeProfileTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MediaWiki\Extension\Speedscope\Tests\Unit;
+
+use MediaWiki\Extension\Speedscope\SpeedscopeProfile;
+use MediaWikiUnitTestCase;
+
+/**
+ * @covers \MediaWiki\Extension\Speedscope\SpeedscopeProfile
+ */
+class SpeedscopeProfileTest extends MediaWikiUnitTestCase {
+
+	public function testConstruct() {
+		$profile = new SpeedscopeProfile( 'test-env', true, 'test-id' );
+		$this->assertEquals( 'test-env', $profile->getEnvironment() );
+		$this->assertTrue( $profile->isForced() );
+		$this->assertEquals( 'test-id', $profile->getId() );
+	}
+
+	public function testGettersAndSetters() {
+		$profile = new SpeedscopeProfile( 'test-env', true, 'test-id' );
+
+		$this->assertNull( $profile->getData() );
+		$data = [ 'test' => 'data' ];
+		$profile->setData( $data );
+		$this->assertEquals( $data, $profile->getData() );
+
+		$this->assertNull( $profile->getParserReport() );
+		$parserReport = [ 'test' => 'parserReport' ];
+		$profile->setParserReport( $parserReport );
+		$this->assertEquals( $parserReport, $profile->getParserReport() );
+
+		$this->assertFalse( $profile->shouldStoreParserReport() );
+		$profile->setStoreParserReport( true );
+		$this->assertTrue( $profile->shouldStoreParserReport() );
+	}
+
+}
